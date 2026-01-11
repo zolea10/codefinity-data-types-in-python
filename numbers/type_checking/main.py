145319e4ee2a3e2c90953f829fd2d7dd035640b3
zolea10 <>
2026-01-11 17:07:37 +0000

    Type Checking

---
+++ numbers/type_checking/main.py
@@ -0,0 +1,8 @@
+number1 = 88/16e-1
+number2 = 12*2
+
+# Print the number and it's type
+print("The value of number1 is",number1)
+print("The type of number1 is", type(number1))
+print("The value of number2 is", number2)
+print("The type of number2 is", type(number2))
